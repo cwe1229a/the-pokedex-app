bbd086148573294a65ddb9d20d863a4e814b76e9
Courtney Webster <courtney@atlmd.net>
2022-03-27 12:52:14 -0400

    updated loop in js for pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 let pokemonList = [
 { name: "bulbasaur", height: 2, type: ["grass", "poison"]},
-{ name: "charmander", height: 2, type:["fire"]},
+{ name: "charmander", height: 3, type:["fire"]},
 { name: "squirtle", height: 1, type: ["water"]}
 ];
 // repository for pokemon
 
 for (let i=0; i <pokemonList.length; i++){
   if (pokemonList[i].height >2){
-    document.write(pokemonList[i].height + "That's big! <br />");
-}
-{
-  document.write(pokemonList[i].name + "<br />");
-  document.write(pokemonList[i].height + "<br />");
-  document.write(pokemonList[i].type + "<br />");
+    document.write(pokemonList[i].name + ": </br> " + pokemonList[i].type + " </br>" + pokemonList[i].height + " - " + "That's big! <br/>");
+} else {
+    document.write(pokemonList[i].name + ": </br> " + pokemonList[i].type + " <br/>" + pokemonList[i].height + "<br/>");
 }
 }
+
 //for loop for how big the pokemon are
