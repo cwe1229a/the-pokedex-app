511bc50662fb9fc233eb108877c505c59a75c5e5
Courtney Webster <courtney@atlmd.net>
2022-03-26 21:09:48 -0400

    added colors to css, added loop to js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 let pokemonList = [
-{ name: "bulbasaur", height: 2.04, type: ["grass", "poison"]},
-{ name: "charmander", height: 2.05, type: "fire"},
-{ name: "squirtle", height: 1.08, type: "water"}
+{ name: "bulbasaur", height: 2, type: ["grass", "poison"]},
+{ name: "charmander", height: 2, type:["fire"]},
+{ name: "squirtle", height: 1, type: ["water"]}
 ];
+// repository for pokemon
+
+for (let i=0; i <pokemonList.length; i++){
+  if (pokemonList[i].height >2){
+    document.write(pokemonList[i].height + "That's big! <br />");
+}
+{
+  document.write(pokemonList[i].name + "<br />");
+  document.write(pokemonList[i].height + "<br />");
+  document.write(pokemonList[i].type + "<br />");
+}
+}
+//for loop for how big the pokemon are
